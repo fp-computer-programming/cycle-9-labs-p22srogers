cfc2bb7d39b36294330dec3de859fc3d24b2923b
P22srogers <p22srogers@fairfieldprep.org>
2022-01-13 11:51:00 -0500

    sub

--- lab_2-1.py
+++ lab_2-1.py
@@ -8,9 +8,8 @@ def test_case():
     l2=[1.1,2,3.14,4]
     l3=[1,"a",2,"b"]
 
-print(double_stuff(l1)==[2,4,6,8])
-print(double_stuff(l2)==[2.2,4,6.28,8])
-print(double_stuff(l3)==[2,"aa",4,"bb"])
+print(double_stuff([1,2,3,4])==[2,4,6,8])
+print(double_stuff([1.1,2,3.14,4])==[2.2,4,6.28,8])
+print(double_stuff([1,"a",2,"b"])==[2,"aa",4,"bb"])
 
 test_case()
-# Getting Name Error: 'l1' is not defined.. not sure why considering I defined in the previous lines
